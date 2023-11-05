9d2e7dc044c4b69333936df6af775e6aabe52936
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-11-05 21:39:34 +0100

    createGitIgnoreAndModuleInfoFiles

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module WeatherAppJavaFX {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens com.test;
+    opens com.test.view;
+    opens com.test.controller;
+    opens com.test.model;
+}
