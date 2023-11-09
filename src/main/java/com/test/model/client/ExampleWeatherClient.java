421a99c1347a4e08be1f37ab0b959a0fcdec37a7
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-11-09 21:23:06 +0100

    correctionInMainViewfxml

--- src/main/java/com/test/model/client/ExampleWeatherClient.java
+++ src/main/java/com/test/model/client/ExampleWeatherClient.java
@@ -3,10 +3,19 @@ package com.test.model.client;
 import com.test.model.Weather;
 
 import java.time.LocalDate;
+//import org.apache.http.client.HttpClient;
 
 public class ExampleWeatherClient implements WeatherClient{
     @Override
     public Weather getWeather(String cityName) {
+
+        //HttpClient httpClient = HttpClients.createDefault();
+
+
+
+
+
+
         return new Weather(cityName,10, LocalDate.now());
     }
 }
