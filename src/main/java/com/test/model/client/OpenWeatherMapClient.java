ec9b54b3bd57ada4cced8a1aff5504dc14b918a0
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-12-28 21:26:34 +0100

    addTestsForModel#2

--- src/main/java/com/test/model/client/OpenWeatherMapClient.java
+++ src/main/java/com/test/model/client/OpenWeatherMapClient.java
@@ -38,14 +38,26 @@ public class OpenWeatherMapClient implements WeatherClient{
         hourAndMinutes = LocalDateTime.now().format(timeFormat);
 
         try {
-            weatherNow = callGetMethod(String.class,"weather",cityName,countryCode,Config.API_KEY);
+            weatherNow = restTemplate.getForObject(WEATHER_URL + "{typeOfWeather}?q={cityName},{countryCode}&appid={apiKey}&units=metric&lang=pl",
+                    String.class, "weather",cityName,countryCode,Config.API_KEY);
+            if (weatherNow == null) {
+                throw new NullPointerException("No weather found for this city");
+            }
             JsonNode jsonNode = objectMapper.readTree(weatherNow);
             descriptionWeather = jsonNode.get("weather").get(0).get("description").asText();
             iconWeatherCode = jsonNode.get("weather").get(0).get("icon").asText();
             temperature = jsonNode.get("main").get("temp").asDouble();
             feelsLikeTemperature = jsonNode.get("main").get("feels_like").asDouble();
+            System.out.println(temperature);
+            System.out.println(hourAndMinutes);
+            System.out.println(iconWeatherCode);
+            System.out.println(iconWeatherCode);
+            System.out.println(descriptionWeather);
+            System.out.println(feelsLikeTemperature);
+            System.out.println(cityName);
         } catch (IOException e) {
             e.printStackTrace();
+            throw new NullPointerException("No weather found for this city");
         }
         return new Weather(temperature, hourAndMinutes, iconWeatherCode, descriptionWeather, feelsLikeTemperature,cityName);
     }
@@ -91,7 +103,7 @@ public class OpenWeatherMapClient implements WeatherClient{
 
         try {
             weatherForecast = callGetMethod(String.class,"forecast",cityName,countryCode,Config.API_KEY);
-            System.out.println("callGetMethod::: " + weatherForecast);
+
             JsonNode jsonNode = objectMapper.readTree(weatherForecast);
             Iterator<JsonNode> forecastNodeIterator = jsonNode.get("list").iterator();
 
@@ -137,6 +149,13 @@ public class OpenWeatherMapClient implements WeatherClient{
     private <T> T callGetMethod (Class <T> responseType,Object...objects) {
         return restTemplate.getForObject(WEATHER_URL + "{typeOfWeather}?q={cityName},{countryCode}&appid={apiKey}&units=metric&lang=pl",
                 responseType, objects);
+//        String answer = "{\"weather\":[{\"description\":\"zachmurzenie duże\",\"icon\":\"04n\"}],\"main\":{\"temp\":2.78,\"feels_like\":-0.01}}";
+//        return answer;
+    }
+
+    public String callGetMethodString (Object...objects) {
+        return restTemplate.getForObject(WEATHER_URL + "{typeOfWeather}?q={cityName},{countryCode}&appid={apiKey}&units=metric&lang=pl",
+                String.class, objects);
     }
 
     public static String formatWithTodayYesterdayHourAndMinutes(String dateTime) {
--- src/test/java/com/test/model/client/OpenWeatherMapClientTest.java
+++ src/test/java/com/test/model/client/OpenWeatherMapClientTest.java
@@ -1,5 +1,6 @@
 package com.test.model.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.model.Weather;
 import com.test.model.WeatherService;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,15 +8,53 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 public class OpenWeatherMapClientTest{
 
+    String cityName = "Gdańsk";
+    String countryCode = "PL";
+    String WEATHER_URL = "https://api.openweathermap.org/data/2.5/";
+    private OpenWeatherMapClient weatherClient;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Mock
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        weatherClient = new OpenWeatherMapClient(restTemplate, objectMapper);
+    }
+
+    @Test
+    void shouldReturnWeatherMockInside() {
+        //given
+
+        String preparedAnswer = "{\"weather\":[{\"description\":\"zachmurzenie duże\",\"icon\":\"04n\"}],\"main\":{\"temp\":2.78,\"feels_like\":-0.01}}";
+        when(restTemplate.getForObject(WEATHER_URL + "{typeOfWeather}?q={cityName},{countryCode}&appid={apiKey}&units=metric&lang=pl",
+                String.class, "weather",cityName,countryCode,Config.API_KEY))
+                .thenReturn(preparedAnswer);
+
+        //when
+        Weather result = weatherClient.getWeather("Gdańsk","Polska");
+        //then
+        assertThat(result.getTemperature()).isEqualTo(2.78);
+        assertThat(result.getIconWeatherCode()).isEqualTo("04n");
+        assertThat(result.getDescriptionWeather()).isEqualTo("zachmurzenie duże");
+        assertThat(result.getFeelsLikeTemperature()).isEqualTo(-0.01);
+        assertThat(result.getCityName()).isEqualTo("Gdańsk");
+
+    }
+
+
+    /*
     @Mock
     private WeatherClient weatherClient;
 
@@ -23,12 +62,6 @@ public class OpenWeatherMapClientTest{
     public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
-
-//    @Test
-//    void shouldReturnWeatherMockInside() {
-//
-//    }
-
     @Test
     void shouldReturnWeather() {
         //given
@@ -95,7 +128,7 @@ public class OpenWeatherMapClientTest{
             weatherClient.getWeather("Gdańsk", "Polska");
         });
     }
-
+    */
     private Weather createWeather() {
         return new Weather(10, "12:00", "10e", "Słonecznie", 11, "Gdańsk");
     }
@@ -104,5 +137,10 @@ public class OpenWeatherMapClientTest{
     }
     private Weather createForecastDaily() {
         return new Weather(10, 2,"Sobota", "10e");
+    }
+
+    private String preparedAnswer() {
+        String answer = "{\"coord\":{\"lon\":18.6464,\"lat\":54.3521},\"weather\":[{\"id\":804,\"main\":\"Clouds\",\"description\":\"zachmurzenie duże\",\"icon\":\"04n\"}],\"base\":\"stations\",\"main\":{\"temp\":2.78,\"feels_like\":-0.01,\"temp_min\":2.27,\"temp_max\":3.13,\"pressure\":1018,\"humidity\":80},\"visibility\":10000,\"wind\":{\"speed\":2.82,\"deg\":226,\"gust\":4.49},\"clouds\":{\"all\":100},\"dt\":1703711685,\"sys\":{\"type\":2,\"id\":2089861,\"country\":\"PL\",\"sunrise\":1703660786,\"sunset\":1703687150},\"timezone\":3600,\"id\":3099434,\"name\":\"Gdańsk\",\"cod\":200}";
+        return answer;
     }
 }
